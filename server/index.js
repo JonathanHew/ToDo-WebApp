d2d3935e997d1807be90dcbd8f0223b1122bbb19
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2022-10-12 17:21:03 +0100

    create todo route implemented and tested using Postman

--- server/index.js
+++ server/index.js
@@ -1,25 +1,40 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-//we use pool to make PostgreSQL queries 
+//we use pool to make PostgreSQL queries
 const pool = require("./db");
 
 //middleware
 app.use(cors());
+//this line lets us use req.body in requests
 app.use(express.json());
 
 //ROUTES//
 
-//create a todo
+//create a todo, post is used because we are adding data
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES ($1) RETURNING * ",
+      [description]
+    );
+    
+    //newTodo.rows[0] returns only the necessary data, "todo_id" and "description"
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 //get all todos
 
 //get a todo
 
-//update a todo 
+//update a todo
 
 //delete a todo
 
 app.listen(5001, () => {
-    console.log("server has started on port 5001!");
+  console.log("server has started on port 5001!");
 });
