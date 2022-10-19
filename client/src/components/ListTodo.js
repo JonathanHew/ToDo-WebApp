72fe9fb1bd6989c2e1ac8b0730ddebce196a7f48
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2022-10-19 12:48:11 +0100

    deleteTodo function in listTodo component implemented

--- client/src/components/ListTodo.js
+++ client/src/components/ListTodo.js
@@ -15,6 +15,24 @@ const ListTodos = () => {
     }
   };
 
+  //delete todo function
+
+  const deleteTodo = async (id) => {
+    try {
+      const deleteTodo = await fetch(
+        `http://localhost:5001/todos/${id}`,
+        {
+          method: "DELETE",
+        }
+      );
+    
+      //use a filter to return all todos exceot the one which has the id passed into the delete function
+      setTodos(todos.filter(todo => todo.todo_id != id));
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   //useEffect will constantly run which means getTodos() will keep on happening
   //to stop this add [] at the bottom to make it do 1 request only
   useEffect(() => {
@@ -38,10 +56,18 @@ const ListTodos = () => {
                     <td>john@example.com</td>
             </tr>*/}
           {todos.map((todo) => (
-            <tr>
+            //key gives the each row its id from the DB
+            <tr key={todo.todo_id}>
               <td>{todo.description}</td>
               <td>Edit</td>
-              <td>Delete</td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteTodo(todo.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
